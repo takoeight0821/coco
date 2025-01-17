a3397d67c934e61432f86f53538feb138c778d3d
Yuya Kono <takohati0821@gmail.com>
2025-01-17 04:59:41 +0000

    Add devcontainer configuration and Dependabot setup; update flake.nix for Haskell support

--- flake.nix
+++ flake.nix
@@ -19,6 +19,13 @@
           sha256 = "sha256-lMLAupxng4Fd9F1oDw8gx+qA0RuF7ou7xhNU8wgs0PU=";
         };
         pkgs = nixpkgs.legacyPackages.${system};
+        ghc = pkgs.haskell.compiler.ghc910;
+        haskellPackages = pkgs.haskell.packages.ghc910.override {
+          overrides = final: prev: {
+            # distribution-nixpkgs's tests are broken on ghc-9.10
+            distribution-nixpkgs = pkgs.haskell.lib.dontCheck prev.distribution-nixpkgs;
+          };
+        };
       in
       {
         packages.default =
@@ -29,9 +36,12 @@
           };
         formatter = pkgs.nixpkgs-fmt;
         devShells.default = pkgs.stdenv.mkDerivation {
-          name = "rust environment";
+          name = "develop environment";
           nativeBuildInputs = [
             toolchain
+            haskellPackages.cabal-install
+            haskellPackages.cabal2nix
+            haskellPackages.haskell-language-server
           ];
         };
       }
